1d72044c3b2ea1f27928a0a4f514ee91682ee333
Youz Online <guillaume.valadas@gmail.com>
2018-02-11 20:16:02 +0000

    Add login routes & tests

--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -2,19 +2,39 @@
 
 namespace Tests\Feature;
 
-use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use OpenSpaceLife\User;
+use Tests\TestCase;
 
-class UserTest extends TestCase
-{
+class UserTest extends TestCase {
+
 	use RefreshDatabase;
 
-   /** @test */
-   public function a_user_can_register()
-   {
-	   $response = $this->post('/register', ['name' => 'Anna', 'email' => 'anna@lapetie.fr', 'password' => 'password', 'password_confirmation' => 'password']);
+	/** @test */
+	public function a_user_can_register()
+	{
+		$response = $this->post('/register', ['name' => 'Anna', 'email' => 'anna@lapetie.fr', 'password' => 'password', 'password_confirmation' => 'password']);
 
-	   $response->assertRedirect('/home');
-	   $this->assertDatabaseHas('users', ['email' => 'anna@lapetie.fr']);
+		$response->assertRedirect('/home');
+		$this->assertDatabaseHas('users', ['email' => 'anna@lapetie.fr']);
+	}
+
+	/** @test */
+	public function a_user_can_login()
+	{
+		$user = factory(User::class)->raw();
+		$response = $this->post('/login', ['email' => $user['email'], 'password' => $user['password']]);
+
+		$response->assertRedirect('/');
+   }
+
+   /** @test */
+   public function a_user_can_logout()
+   {
+	   $user = factory(User::class)->create();
+
+	   $response = $this->actingAs($user)->post('/logout');
+
+	   $response->assertRedirect('/');
    }
 }
