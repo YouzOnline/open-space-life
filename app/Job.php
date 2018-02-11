56ba9bd6cdff42c24d0da56046e92891527ed207
Youz <guillaume.valadas@gmail.com>
2018-02-11 20:54:14 +0000

    Apply fixes from StyleCI

--- app/Job.php
+++ app/Job.php
@@ -4,12 +4,12 @@ namespace OpenSpaceLife;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Job extends Model {
-
-	/**
-	 * The attributes that are mass assignable.
-	 *
-	 * @var array
-	 */
-	protected $fillable = ['name', 'description', 'team_id'];
+class Job extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['name', 'description', 'team_id'];
 }
